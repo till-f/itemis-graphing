93e5819f92bd04434973ab3cfaba95bae387161a
Till Fischer <till@es-tdk.org>
2017-07-21 15:39:41 +0200

    replaced abego source code with jar, commented out treelayout

--- src/de/itemis/graphstreamwrapper/layout/tree/InternalNodeExtendProvider.java
+++ src/de/itemis/graphstreamwrapper/layout/tree/InternalNodeExtendProvider.java
@@ -1,20 +1,20 @@
-package de.itemis.graphstreamwrapper.layout.tree;
-
-import de.itemis.graphstreamwrapper.layout.InternalNode;
-import org.abego.treelayout.NodeExtentProvider;
-
-public class InternalNodeExtendProvider implements  NodeExtentProvider<InternalNode>
-{
-    @Override
-    public double getWidth(InternalNode treeNode)
-    {
-        return treeNode.getWidth();
-    }
-
-    @Override
-    public double getHeight(InternalNode treeNode)
-    {
-        return treeNode.getHeight();
-    }
-}
-
+//package de.itemis.graphstreamwrapper.layout.tree;
+//
+//import de.itemis.graphstreamwrapper.layout.InternalNode;
+//import org.abego.treelayout.NodeExtentProvider;
+//
+//public class InternalNodeExtendProvider implements  NodeExtentProvider<InternalNode>
+//{
+//    @Override
+//    public double getWidth(InternalNode treeNode)
+//    {
+//        return treeNode.getWidth();
+//    }
+//
+//    @Override
+//    public double getHeight(InternalNode treeNode)
+//    {
+//        return treeNode.getHeight();
+//    }
+//}
+//
--- src/de/itemis/graphstreamwrapper/layout/tree/TreeLayoutAbego.java
+++ src/de/itemis/graphstreamwrapper/layout/tree/TreeLayoutAbego.java
@@ -1,65 +1,65 @@
-package de.itemis.graphstreamwrapper.layout.tree;
-
-import de.itemis.graphstreamwrapper.layout.InternalNode;
-import de.itemis.graphstreamwrapper.layout.StaticLayout;
-import org.abego.treelayout.util.DefaultConfiguration;
-import org.abego.treelayout.util.DefaultTreeForTreeLayout;
-
-public class TreeLayoutAbego extends StaticLayout
-{
-    protected final DefaultConfiguration<InternalNode> _layouterConfiguration;
-    protected final InternalNodeExtendProvider _nodeExtentProvider;
-
-    protected double _gapBetweenLevels;
-    protected double _gapBetweenNodes;
-
-    public TreeLayoutAbego()
-    {
-        this(0.5, 0.5);
-    }
-
-    public TreeLayoutAbego(double gapBetweenLevels, double gapBetweenNodes)
-    {
-        _gapBetweenLevels = gapBetweenLevels;
-        _gapBetweenNodes = gapBetweenNodes;
-        _layouterConfiguration = new DefaultConfiguration<InternalNode>(gapBetweenLevels, gapBetweenNodes);
-        _nodeExtentProvider = new InternalNodeExtendProvider();
-    }
-
-    @Override
-    public String getLayoutAlgorithmName()
-    {
-        return "Tree Layout";
-    }
-
-    @Override
-    protected void computeLayout()
-    {
-        double xOffset = 0;
-        for (InternalNode root : getRootNodes())
-        {
-            DefaultTreeForTreeLayout<InternalNode> tree = new DefaultTreeForTreeLayout<InternalNode>(root);
-            fillAbegoTree_recursive(tree, root);
-
-            org.abego.treelayout.TreeLayout<InternalNode> treeLayout = new org.abego.treelayout.TreeLayout<InternalNode>(tree, _nodeExtentProvider, _layouterConfiguration);
-            for(InternalNode n : treeLayout.getNodeBounds().keySet())
-            {
-                n.place(treeLayout.getNodeBounds().get(n).x + xOffset, treeLayout.getNodeBounds().get(n).y * -1);
-            }
-            xOffset += treeLayout.getBounds().getWidth() + _gapBetweenNodes;
-        }
-    }
-
-    private void fillAbegoTree_recursive(DefaultTreeForTreeLayout<InternalNode> tree, InternalNode parent)
-    {
-        for(InternalNode child : parent.getTargets())
-        {
-            if (tree.hasNode(child))
-                continue;
-
-            tree.addChildren(parent, child);
-            fillAbegoTree_recursive(tree, child);
-        }
-    }
-
-}
+//package de.itemis.graphstreamwrapper.layout.tree;
+//
+//import de.itemis.graphstreamwrapper.layout.InternalNode;
+//import de.itemis.graphstreamwrapper.layout.StaticLayout;
+//import org.abego.treelayout.util.DefaultConfiguration;
+//import org.abego.treelayout.util.DefaultTreeForTreeLayout;
+//
+//public class TreeLayoutAbego extends StaticLayout
+//{
+//    protected final DefaultConfiguration<InternalNode> _layouterConfiguration;
+//    protected final InternalNodeExtendProvider _nodeExtentProvider;
+//
+//    protected double _gapBetweenLevels;
+//    protected double _gapBetweenNodes;
+//
+//    public TreeLayoutAbego()
+//    {
+//        this(0.5, 0.5);
+//    }
+//
+//    public TreeLayoutAbego(double gapBetweenLevels, double gapBetweenNodes)
+//    {
+//        _gapBetweenLevels = gapBetweenLevels;
+//        _gapBetweenNodes = gapBetweenNodes;
+//        _layouterConfiguration = new DefaultConfiguration<InternalNode>(gapBetweenLevels, gapBetweenNodes);
+//        _nodeExtentProvider = new InternalNodeExtendProvider();
+//    }
+//
+//    @Override
+//    public String getLayoutAlgorithmName()
+//    {
+//        return "Tree Layout";
+//    }
+//
+//    @Override
+//    protected void computeLayout()
+//    {
+//        double xOffset = 0;
+//        for (InternalNode root : getRootNodes())
+//        {
+//            DefaultTreeForTreeLayout<InternalNode> tree = new DefaultTreeForTreeLayout<InternalNode>(root);
+//            fillAbegoTree_recursive(tree, root);
+//
+//            org.abego.treelayout.TreeLayout<InternalNode> treeLayout = new org.abego.treelayout.TreeLayout<InternalNode>(tree, _nodeExtentProvider, _layouterConfiguration);
+//            for(InternalNode n : treeLayout.getNodeBounds().keySet())
+//            {
+//                n.place(treeLayout.getNodeBounds().get(n).x + xOffset, treeLayout.getNodeBounds().get(n).y * -1);
+//            }
+//            xOffset += treeLayout.getBounds().getWidth() + _gapBetweenNodes;
+//        }
+//    }
+//
+//    private void fillAbegoTree_recursive(DefaultTreeForTreeLayout<InternalNode> tree, InternalNode parent)
+//    {
+//        for(InternalNode child : parent.getTargets())
+//        {
+//            if (tree.hasNode(child))
+//                continue;
+//
+//            tree.addChildren(parent, child);
+//            fillAbegoTree_recursive(tree, child);
+//        }
+//    }
+//
+//}
