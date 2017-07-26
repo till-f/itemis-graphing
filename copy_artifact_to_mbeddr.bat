24571263e31082e9b1a51fac655b7daf0ad6ef04
Till Fischer <till@es-tdk.org>
2017-07-26 09:52:56 +0200

    ...

--- copy_artifact_to_mbeddr.bat
+++ copy_artifact_to_mbeddr.bat
@@ -1,1 +1,1 @@
-copy "C:\git\mps.graphstreamwrapper\out\artifacts\graphstreamwrapper\graphstreamwrapper.jar" "C:\git\mbeddr.core\code\languages\com.mbeddr.mpsutil\languages\com.mbeddr.mpsutil.graphstream\runtime\lib\"
+copy "C:\git\de.itemis.graphing\out\artifacts\de.itemis.graphing\de.itemis.graphing.jar" "C:\git\mbeddr.core\code\languages\com.mbeddr.mpsutil\languages\com.mbeddr.mpsutil.graphstream\runtime\lib\"
